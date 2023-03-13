bb0d08d5764bf11c01eb52c27786a2b70f4a98fc
GIDEON MAINA <mutondogm@gmail.com>
2023-03-13 11:53:25 +0300

    button

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ function clearFields() {
     emailField.value = "";
     subjectField.value = "";
     messageField.value = "";
+
+    stateHandle();
+}
+
+
+let input = document.querySelector(".input");
+let button = document.querySelector(".button");
+
+button.disabled = true; //setting button state to disabled
+
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+  if (document.querySelector(".input").value === "") {
+    button.disabled = true; //button remains disabled
+  } else {
+    button.disabled = false; //button is enabled
+  }
 }
 
 // Success Alert
